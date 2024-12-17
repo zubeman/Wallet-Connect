8463238ce7239528fef4366a425c9d11d9114cf9
Chris Zube <64733627+zubeman@users.noreply.github.com>
2024-12-17 13:32:17 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,9 @@
-const db = require('../config/db.config');
-const mysql = require('mysql');
+const pool = require('../config/db.config');
 
 class User {
   static create(username, password) {
     return new Promise((resolve, reject) => {
-      db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (error, result) => {
+      pool.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (error, result) => {
         if (error) return reject(error);
         resolve(result.insertId);
       });
@@ -13,7 +12,7 @@ class User {
 
   static findByUsername(username) {
     return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM users WHERE username = ?', [username], (error, results) => {
+      pool.query('SELECT * FROM users WHERE username = ?', [username], (error, results) => {
         if (error) return reject(error);
         resolve(results[0]);
       });
