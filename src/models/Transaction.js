de543d3fce9e613fbbb8f2f01638eb4ebf8266fb
Chris Zube <64733627+zubeman@users.noreply.github.com>
2024-12-17 13:25:57 +0100

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,10 +1,23 @@
-const db = require('../config/db.config');
+// In Transaction.js
+const mysql = require('mysql');
+const dbConfig = require('../config/db.config');
+
+const db = mysql.createConnection(dbConfig);
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Transaction model connected to MySQL database');
+});
 
 class Transaction {
-  static log(userId, txHash, amount) {
+  static async log(userId, txHash, amount) {
     return new Promise((resolve, reject) => {
       db.query('INSERT INTO transactions (user_id, tx_hash, amount) VALUES (?, ?, ?)', [userId, txHash, amount], (error, result) => {
-        if (error) return reject(error);
+        if (error) reject(error);
         resolve(result.insertId);
       });
-});
+    });
+  }
+}
+
+module.exports = Transaction;
