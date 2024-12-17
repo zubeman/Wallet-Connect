b81d5d5cb149572b9c2114b2f931539b735c486c
Chris Zube <64733627+zubeman@users.noreply.github.com>
2024-12-17 11:05:07 +0100

    Create wc.config.js

---
+++ src/config/wc.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projectId: process.env.WALLET_CONNECT_PROJECT_ID,
+  metadata: {
+    name: "Secure Wallet Connect App",
+    description: "Connect, Authenticate, and Securely Transfer",
+    url: '#',
+    icons: ['path_to_icon']
+  }
+};
